a31b469f7da50d53725536e31086634a71e410f1
PreciousAma <preshchilaka06@gmail.com>
2023-05-22 21:59:38 +0100

    add search component to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AppBar as MuiAppBar, Toolbar, Link, styled } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import logo from '../assets/pancakes_bg.png';
 
 export const NavBar = () => {
 	return (
@@ -8,9 +10,13 @@ export const NavBar = () => {
 				<NavLink href='/'>Home</NavLink>
 				<NavLink href='/'>About us</NavLink>
 				<NavLink href='/'>Shop</NavLink>
+				<NavLink href='/'>
+					<img src={logo} alt='Amara Bakes' className='logo' />
+				</NavLink>
 				<NavLink href='/'>Pages</NavLink>
 				<NavLink href='/'>Blog</NavLink>
 				<NavLink href='/'>Contact</NavLink>
+				<Search fontSize='100px' />
 			</Toolbar>
 		</AppBar>
 	);
@@ -18,19 +24,29 @@ export const NavBar = () => {
 
 const AppBar = styled(MuiAppBar)(
 	({ theme }) => `
+	display: flex;
+	justify-content: space-around;
 	background-color: ${theme.palette.background.paper};
+
+	& .MuiToolbar-root {
+		display: flex;
+		justify-content: center;
+	}
 `
 );
 
 const NavLink = styled(Link)(
 	({ theme }) => `
     color: ${theme.palette.text.navlink};
-	display: flex;
     padding: 30px;
 	padding-bottom: 10px;
     font-size: 16px;
     text-decoration: none;
 	align-items: center;
+
+	& .logo {
+		width: 90px;
+	}
 
 	:hover {
 		border-bottom: 2px solid ${theme.palette.primary.main};
